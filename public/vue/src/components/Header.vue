60718d608a2a9a9d400549d6a09de25b78f851ea
s <99058368@mydavinci.nl>
2022-04-25 11:54:51 +0100

    header component for spa index

---
+++ public/vue/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="huge_banner">
+        <img src="" alt="guitar" id="guitar">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .huge_banner{
+        margin-top: 10px;
+        margin-right: 10px;
+        height: 400px;
+        background-color: #fbc84f;
+    }
+</style>
