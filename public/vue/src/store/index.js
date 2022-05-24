9539a75091861d924b4e30ee729b069ca090d815
s <99058368@mydavinci.nl>
2022-05-24 09:43:19 +0100

    Handle request to login to API

--- public/vue/src/store/index.js
+++ public/vue/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
 
 Vue.use(Vuex)
+
+axios.defaults.baseURL = 'http://juke.nl/api/'
+axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem("token");
 
 export default new Vuex.Store({
   state: {
@@ -9,6 +14,21 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+      //User Login
+      login(context, data){
+          return new Promise((resolve, reject) => {
+              //Post request
+              axios.post('user/validate', {
+                  name: data[0],
+                  password: data[1]
+              //Result
+              }).then(response => {
+                  resolve(response.data)
+              }).catch(response =>{
+                  reject(response)
+              })
+          })
+      },
   },
   modules: {
   }
