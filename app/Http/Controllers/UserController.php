1b74ae057b198f9e174bba23a1bbf819ce5d1fe7
s <99058368@mydavinci.nl>
2022-05-24 09:41:26 +0100

    validate returned info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class UserController extends Controller
 {
     //
-    public function demo(){
-        return 'lol';
+    public function user_Login(Request $request){
+        //Validate request
+        $field = $request->validate([
+            'name' => 'required|string|min:3',
+            'password' => 'required|string|min:3'
+        ]);
+        //Check if entry exists
+        $user = User::where('name', $field['name'])->first();
+
+        //If entry doesn't exist, return false
+        if(!$user || !Hash::check($field['password'], $user->password)){
+            //Return false
+            return response([
+                'message' => 'Failed to login.'
+            ],401);
+        }
+
+        //Create token
+        $token = $user->createToken('web')->plainTextToken;
+
+        //Return data
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return response($response, 201);
     }
+
     public function user_register(Request $request){
         //Validate login fields
         $field = $request->validate([
