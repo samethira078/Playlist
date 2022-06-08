a208dd0703fae5f2aa279c8b54848a0c93a89125
s <99058368@mydavinci.nl>
2022-06-08 21:35:43 +0100

    setting relationship

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Artist extends Model
 {
     use HasFactory;
+
+    public function songs()
+    {
+        return $this->belongsTo('App\Models\Song');
+    }
 }
--- app/Models/Song.php
+++ app/Models/Song.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Song extends Model
 {
     use HasFactory;
+
+    public function artist(){
+        return $this->hasOne(Artist::class, 'id', 'artist_id');
+    }
+    public function genre(){
+        return $this->hasOne(Genre::class, 'id', 'genre_id');
+    }
 }
