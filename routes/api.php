70326d231c02f8aa64b8c42e6c9067d933116f5a
s <99058368@mydavinci.nl>
2022-06-09 18:33:06 +0100

    path to edit playlist

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+Use App\Http\Controllers\Songs;
+use App\Http\Controllers\PlaylistController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +21,12 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/demo', function () {
         return 'Hello World';
     });
+    Route::get('/user/playlist', [PlaylistController::class, 'getPlaylist']);
+    Route::post('/user/playlist/songs', [PlaylistController::class, 'getPlaylistSongs']);
+    Route::post('/user/add/song', [PlaylistController::class, 'addSongToPlaylist']);
+    Route::post('/user/remove/song', [PlaylistController::class, 'removeSongFromPlaylist']);
 });
 
 Route::post('/user/validate', [UserController::class, 'user_Login']);
 Route::post('/user/register', [UserController::class, 'user_register']);
+Route::get('/songs/all', [Songs::class, 'list']);
