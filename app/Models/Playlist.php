f3da5a615fdcff1c8131c457a9d6158fe46b86be
s <99058368@mydavinci.nl>
2022-06-09 23:28:22 +0100

    editable

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -8,7 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Playlist extends Model
 {
     use HasFactory;
-
+    public $timestamps = false;
+    protected $fillable = [
+        'name',
+        'user_id',
+    ];
     public function playlistsong(){
         return $this->belongsTo('App\Models\PlaylistSong');
     }
